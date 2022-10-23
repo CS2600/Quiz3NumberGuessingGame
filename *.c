63d28e5a2be6ac46d768f1c8fdb8b9fd4750d532
jhhocs <joshuaho1@cpp.edu>
2022-10-22 21:05:56 -0700

    Added outline

---
+++ *.c
@@ -0,0 +1,27 @@
+//Print start menu information
+
+//Read user input
+
+//****Option 1****//
+
+//Randomize number between 1 and the max number
+
+//If the user gets the number correct, print that they won and return to menu
+
+//****Option 2****//
+
+//Print max value user can set the number
+
+//Check if the number they provide is negative or greater than the max value
+
+//Update the max value
+
+//****Option 3****//
+
+//End program by calling return
+
+//****save_user_max_number****//
+
+//Save max_number to a text file.
+
+//To access, check if file exists, and if it does, set max_number to the number in the text file.
