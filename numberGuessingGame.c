18a7a4490a447e71a40913966b8875d4c91a35c2
jhhocs <joshuaho1@cpp.edu>
2022-10-22 23:57:18 -0700

    Added playGame function

--- numberGuessingGame.c
+++ numberGuessingGame.c
@@ -1,41 +1,86 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <time.h>
+#include <ctype.h>
+#include <stdbool.h>
 
-const int MAX_NUMBER = 100;
+const int MAX_VALUE = 100;
 
-void playGame() {
+void playGame(int maxNumber) {
+    //Pick random number from 1 to maxNumber
+    time_t t;
+    srand((unsigned)time(&t));
+    int number = (rand() % maxNumber) + 1;
+    char input[3];
 
+    //Loop until user finds number or inputs q
+    for(;;) {
+
+        printf("Enter a number!\n");
+        scanf("%s", input);
+        fflush(stdin);
+
+        int inputNumber = -1;
+
+        sscanf(input, "%d", &inputNumber);
+        fflush(stdin);
+
+        if(inputNumber != -1) {
+            if(inputNumber == number) {
+                printf("You win!\n");
+                return;
+            }
+            else if(inputNumber < number) {
+                printf("Too Low!\n");
+                continue;
+            }
+            else {
+                printf("Too High!\n");
+                continue;
+            }
+
+        }
+        else if(input[0] == 'q') {
+            return;
+        }
+        else {
+            printf("Invalid input!\n");
+        }
+    }
 }
 
-void changeMaxNumber() {
+void changeMaxNumber(int* maxNumber) {
 
 }
 
 
 int main() {
 
-    printf("Press 1 to play a game\n");
-    printf("Press 2 to change the max number\n");
-    printf("Press 3 to quit\n");
+    int temp = MAX_VALUE;
+    int* maxNumber = &temp;
 
-    int option;
     for(;;) {
-        scanf("%d", &option);
-        if(option != 1 && option != 2 && option != 3) {
-            printf("Please provide a valid input\n");
+        printf("Press 1 to play a game\n");
+        printf("Press 2 to change the max number\n");
+        printf("Press 3 to quit\n");
+
+        int option;
+        for(;;) {
+            scanf("%d", &option);
             fflush(stdin);
-            continue;
+            if(option != 1 && option != 2 && option != 3) {
+                printf("Please provide a valid input\n");
+                continue;
+            }
+            break;
         }
-        break;
-    }
 
-    for(;;) {
         switch(option) {
             case 1: //Play game
-                playGame();
+                playGame(*maxNumber);
                 break;
             case 2: //Change max number
-                changeMaxNumber();
+                changeMaxNumber(maxNumber);
                 break;
             default: //Quit
                 return 1;
