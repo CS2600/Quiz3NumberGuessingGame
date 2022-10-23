760750724efe0894d51f13b1423e12f8b3706daa
jhhocs <joshuaho1@cpp.edu>
2022-10-23 00:18:13 -0700

    Added comments

--- numberGuessingGame.c
+++ numberGuessingGame.c
@@ -16,16 +16,18 @@ void playGame(int maxNumber) {
     //Loop until user finds number or inputs q
     for(;;) {
 
+        //Read user input
         printf("Enter a number!\n");
         scanf("%s", input);
         fflush(stdin);
 
         int inputNumber = -1;
 
+        //Convert char array to int
         sscanf(input, "%d", &inputNumber);
         fflush(stdin);
 
-        if(inputNumber != -1) {
+        if(inputNumber != -1) { //If input is a number
             if(inputNumber == number) {
                 printf("You win!\n\n");
                 return;
@@ -40,32 +42,35 @@ void playGame(int maxNumber) {
             }
 
         }
-        else if(input[0] == 'q') {
+        else if(input[0] == 'q') { //If input is q
             return;
         }
         else {
             printf("Invalid input!\n");
         }
     }
-}
+} //End playGame
 
 void changeMaxNumber(int* maxNumber) {
     printf("The max value that you can set the number to is %d\n", MAX_VALUE);
 
+    //Loop until user provides valid input
     for(;;) {
         int input;
         scanf("%d", &input);
         fflush(stdin);
+
+        //Check if input is valid
         if(input < 0 || input > MAX_VALUE) {
             printf("Please provide a value between 0 and %d\n", MAX_VALUE);
             continue;
         }
+
         *maxNumber = input;
         printf("\n");
         return;
     }
-    
-}
+} //End changeMaxNumber
 
 
 int main() {
@@ -73,16 +78,18 @@ int main() {
     int temp = MAX_VALUE;
     int* maxNumber = &temp;
 
+    //Loop until quit
     for(;;) {
         printf("Press 1 to play a game\n");
         printf("Press 2 to change the max number\n");
         printf("Press 3 to quit\n\n");
 
+        //Read user input
         int option;
         for(;;) {
             scanf("%d", &option);
             fflush(stdin);
-            if(option != 1 && option != 2 && option != 3) {
+            if(option != 1 && option != 2 && option != 3) { //Check if input is not 1, 2, or 3
                 printf("Please provide a valid input\n");
                 continue;
             }
@@ -90,6 +97,7 @@ int main() {
             break;
         }
 
+        //Execute option selected
         switch(option) {
             case 1: //Play game
                 playGame(*maxNumber);
@@ -101,6 +109,4 @@ int main() {
                 return 1;
         }
     }
-
-    return 1;
-}
+} //End main
